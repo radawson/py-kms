47ad159b88b57d7638dbe4ed179604ceffe1b5a8
Richard A. Dawson <dawsonra@intranet.partridgexing.org>
2020-09-16 10:52:49 -0400

    removed sleep and config touch from kms-server

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ chmod 755 /etc/init.d/kms-server
 sh /etc/init.d/kms-server enable
 
 echo "py-kms installed as a process that starts on boot"
-echo "py-kms should autodetect the LAN bridge IP address automatically"
+echo "py-kms will autodetect the LAN bridge IP address on reboot or restart"
+echo -e "If you change the IP in the GUI, you must still manually restart\nthe kms-server."
 echo -e "\nTo start the server manually, type '/etc/init.d/kms-server start'"
 echo -e "\nTo stop the server, type '/etc/init.d/kms-server stop'"
 echo -e "\nTo restart the server, type '/etc/init.d/kms-server restart'"
